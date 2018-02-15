5c1c7ba6d39af731cde13cb5520a9eec959f5de9
Marcelo Boeira <me@marceloboeira.com>
2018-02-15 22:13:23 +0100

    Remove redundant test

--- tests/integration.sh
+++ tests/integration.sh
@@ -8,9 +8,6 @@ echo "y\n" | confirm-before 'echo result' | grep -q result; if [ $? != 0 ]; then
 printf "  when a 'n' is given it does not run the command - "
 echo "n\n" | confirm-before 'echo result' | grep -q result; if [ $? != 0 ]; then echo "PASS"; else echo "FAIL"; exit 1; fi
 
-printf "  when a 'p' is given it does not run the command - "
-echo "p\n" | confirm-before 'echo result' | grep -q result; if [ $? != 0 ]; then echo "PASS"; else echo "FAIL"; exit 1; fi
-
 printf "  when a complex command is given it gets the output - "
 echo "y\n" | confirm-before 'cat ./tests/resource.txt' | grep -q content; if [ $? != 0 ]; then echo "FAIL"; exit 1; else echo "PASS"; fi
 
