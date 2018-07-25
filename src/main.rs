372415f2b0ce262c0b1b3d7606881afa41b7bfbe
Marcelo Boeira <me@marceloboeira.com>
2018-07-25 16:00:13 +0200

    Fix #3 by printing to tty and not stdout
    When you do something like
    
    >  confirm-before echo "fooo" | grep foo
    
    Nothing was happening (apparently).
    
    Under the hood the grep commands consumes the stdout (where we print the
    "Are you sure?(y/n)"), then it keeps waiting for the stdin flush.
    
    Therefore, we need to write to tty instead of writing to stdout, otherwise
    grep suppress the output... it waits silently for the stdin input.
    
    We use termion to do that, simple and works fine.
    
    Unfortunately, I couldn't find an easy way to test for that... I accept
    suggestions.

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,21 @@ use std::io::{self, Write};
 use std::env;
 use std::process::{Command, Stdio};
 
+extern crate termion;
+
 fn main() {
+    // Use the tty to prevent writing to stdout
+    let mut tty = termion::get_tty().unwrap();
+
     // Collect the command from the input
     let args: Vec<String> = env::args().collect();
     let command: String = args.iter()
                               .skip(1)
                               .fold(String::new(), |acc, item| { acc + " " + item });
 
-    // Confirm
-    print!("Are you sure? (y/n) ");
-    io::stdout().flush().unwrap();
+    // Confirm message
+    writeln!(tty, "Are you sure? (y/n)").unwrap();
+    tty.flush().unwrap();
 
     let mut answer = String::new();
     match io::stdin().read_line(&mut answer) {
