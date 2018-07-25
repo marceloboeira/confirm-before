b8ce0de49083c82c30fc338d3f8da67b40a4767f
Marcelo Boeira <me@marceloboeira.com>
2018-07-25 17:12:01 +0200

    WIP TTY ONLY

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use std::io::{self, Write};
+use std::io::{Write, BufReader};
 use std::env;
 use std::process::{Command, Stdio};
 
 extern crate termion;
+use termion::{color, style};
+use termion::input::TermRead;
 
 fn main() {
     // Use the tty to prevent writing to stdout
@@ -15,13 +17,13 @@ fn main() {
                               .fold(String::new(), |acc, item| { acc + " " + item });
 
     // Confirm message
-    writeln!(tty, "Are you sure? (y/n)").unwrap();
+    writeln!(tty, "{}{}Are you sure? (y/n){}", color::Fg(color::Red), style::Bold, style::Reset).unwrap();
     tty.flush().unwrap();
 
-    let mut answer = String::new();
-    match io::stdin().read_line(&mut answer) {
-        Ok(_) => {
-            if answer == "y\n" {
+    let mut reader = BufReader::new(tty);
+    match TermRead::read_line(&mut reader) {
+        Ok(answer) => {
+            if answer == std::option::Option::Some("y".to_string()) {
                 Command::new("sh")
                     .arg("-c")
                     .arg(command)
@@ -31,10 +33,12 @@ fn main() {
                     .expect("Something went wrong");
             }
             else {
-                println!("Okay, not running!")
+                writeln!(tty, "{}{}Okay, not running!{}", color::Fg(color::Red), style::Bold, style::Reset).unwrap();
             }
         }
-        Err(error) => println!("Something went wrong: {}", error),
+        Err(error) => {
+            //writeln!(tty, "{}{}Something went wrong: {}{}", color::Fg(color::Red), style::Bold, style::Reset, error).unwrap();
+        }
     }
 }
 
